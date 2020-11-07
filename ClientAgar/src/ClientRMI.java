8ca42cea761cf261f2f3563a1cb930e1c95eee16
Daniel <dfsantosbu@unal.edu.co>
2020-11-07 17:04:58 +0100

    chore: remove unused imports
    chore: change name compile client impl library.

--- ClientAgar/src/ClientRMI.java
+++ ClientAgar/src/ClientRMI.java
@@ -1,20 +1,16 @@
-import java.awt.MouseInfo;
-import java.awt.Point;
-import java.net.MalformedURLException;
 import java.rmi.Naming;
-import java.rmi.NotBoundException;
-import java.rmi.RemoteException;
 
-import service.IPlayerRemote;
 
 import processing.core.PApplet;
+import service.IPlayerRemote;
+
 
 public class ClientRMI {
 
 	public static void main(String[] args) {
 		
 		IPlayerRemote stub;
-		try {
+		try {  
 			stub = (IPlayerRemote)Naming.lookup("rmi://localhost:1099/PLM");
 			MapGraphics map = new MapGraphics(stub,stub.registerPlayer("Bobby"));
 			PApplet.runSketch(new String[] {"MapGraphics"}, map);
@@ -22,8 +18,7 @@ public class ClientRMI {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-
+	
 	}
 
 }
