92483c58ee62bbf3178120ade4a79bcd912a2b6d
unknown <eclegv11@gmail.com>
2020-12-08 21:48:53 +0100

    WIP : TextSize of the player proportionnal to the size of the cell

--- ClientAgar/src/displayer/PlayerDisplayer.java
+++ ClientAgar/src/displayer/PlayerDisplayer.java
@@ -6,6 +6,7 @@ import model.Player;
 import model.PlayerCell;
 import processing.core.PApplet;
 import processing.core.PConstants;
+import processing.core.PFont;
 
 public class PlayerDisplayer {
 	public static void draw(Player player, PApplet sketch)
@@ -13,16 +14,17 @@ public class PlayerDisplayer {
 		sketch.pushMatrix();
 		sketch.translate((float)player.getX(), (float)player.getY());
 		
-		int textSize = 1; //(int)(15 - (5f*PlayerCell.CELL_MIN_SIZE/(player.getCell().getRadius()*2)));
+		float textSize = (int)(1 - (5f*PlayerCell.CELL_MIN_SIZE/(player.getCell().getRadius()*2)));
 		
 		
 		Color playerColor = player.getCell().getColor();
-		sketch.fill(playerColor.getRed(), playerColor.getGreen(), playerColor.getBlue());	
+		sketch.fill(playerColor.getRed(), playerColor.getGreen(), playerColor.getBlue());
 		sketch.circle(0, 0, (float)(player.getCell().getRadius())*2);
 		sketch.fill(255);
+		sketch.textAlign(PConstants.CENTER);
 		sketch.textSize(textSize);
-		sketch.text(player.getName(), 0, 0);
-		//sketch.text((int)player.getSize(), 0, 0+textSize);
+		sketch.text(player.getName(), -textSize/6, -textSize);
+		sketch.text((int)player.getSize(), -textSize/6, textSize);
 		
 		sketch.popMatrix();
 	}
