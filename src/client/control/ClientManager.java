4939a77c44e747d8043622fca220abce6333d937
Daniel <dfsantosbu@unal.edu.co>
2020-12-24 07:48:48 +0100

    chore: handle the proper exceptions of the client.

--- src/client/control/ClientManager.java
+++ src/client/control/ClientManager.java
@@ -2,7 +2,13 @@ package client.control;
 
 import client.view.MapGraphics;
 import interfaces.IPlayerRemote;
+
+import java.net.MalformedURLException;
+import java.rmi.ConnectException;
 import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
 import processing.core.PApplet;
 
 public class ClientManager {
@@ -20,8 +26,8 @@ public class ClientManager {
       this.stub = (IPlayerRemote) Naming.lookup("rmi://localhost:1099/PLM");
       MapGraphics map = new MapGraphics(stub, username);
       PApplet.runSketch(new String[] {"MapGraphics"}, map);
-    } catch (Exception e) {
+    } catch (RemoteException | MalformedURLException | NotBoundException e) {
       e.printStackTrace();
-    }
+    } 
   }
 }
